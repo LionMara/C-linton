7d7362888460ff791fc4cfcfcfee841a45c516a7
LionMara <lionofmara@gmail.com>
2023-02-09 08:08:36 +0300

    chapter 1

---
+++ Chapter1_HelloWorld/hello1.c
@@ -0,0 +1,22 @@
+#include  <stdio.h>
+
+/*
+ * hello1.c
+ * by Lion
+ *
+ * creatd on 2023/02/09
+ */
+
+/**
+ * main- main
+ *
+ * Return: always return a zero
+ */
+
+int main()
+{
+	printf("Hello, world!\n");
+	return 0;
+}
+
+/* eof */
---
+++ README.md
@@ -0,0 +1,4 @@
+## C languag is old archaic and sweet.
+My aim is to have a bite at this awe.
+
+# Let's roll.
